f1f558a0141032bc2519bb5f0da66c6b40e8c6b6
peteflorence <pete.florence@gmail.com>
2017-12-05 11:06:49 -0500

    parse move list and save to file

--- parse_move_list.py
+++ parse_move_list.py
@@ -20,4 +20,13 @@ for i,v in enumerate(content):
 			if w=="#":
 				fen_rep_hist[j] = " "
 		fen_rep_hist = "".join(fen_rep_hist)
+		fen_rep_hist = fen_rep_hist.split("startpos ")[1]
+		fen_rep_hist = fen_rep_hist.split(" ")
 		print fen_rep_hist
+
+
+move_list_filename = "game_data/"+str(game_id)+"/move_list.txt"
+text_file = open(move_list_filename, "w")
+for i,v in enumerate(fen_rep_hist):
+  text_file.write(str(v)+"\n")
+text_file.close()
